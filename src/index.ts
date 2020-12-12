c0e1e1b07410ea8ea08f923a4f6245cfbbf9175f
hamny88 <ellie.hahm0502@gmail.com>
2020-12-13 03:34:16 +0900

    Genesis Block - initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { throws } from "assert";
+
+class Block {
+    public index:number;
+    public hash:string;
+    public previousHash:string;
+    public data: string;
+    public timestamp: number;
+    constructor(index:number, hash:string, previousHash:string, data:string, timestamp:number) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    };
+}
+const genesisBlock:Block = new Block(0,"4323432","","Hello",123456);
+
+//check to push only Block in blockchain
+let blockChain: [Block] = [genesisBlock];
+
+console.log(blockChain)
+
+export {};
